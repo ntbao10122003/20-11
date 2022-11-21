7f8a72a20eba5bfa5ca93c0a18ceac5e42541398
baontph <baontph21353@gmail.com>
2022-11-21 18:38:02 +0700

    20/11

---
+++ thiep-mung-main/thiep-mung-main/config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Cô ÁNH ';
+const giftUrl = 'https://www.facebook.com/dang.n.anh.3';
+const eventName = 'Chúc Mừng Ngày 20-11';
+const titleCard = 'cô giáo';
+const contentCard = 'Ngày Nhà giáo Việt nam 20/11 là ngày mà các thế hệ học sinh, tỏ lòng thành tri ân, niềm biết ơn đối với thầy cô giáo đã dìu dắt, dạy dỗ dành cả tâm huyết cho sự nghiệp trồng người cao cả, góp phần xây dựng nguồn nhân lực, kiến tạo đất nước.';
+
+
+const giftImage = '106256033_10207321349149897_5246534646093174756_n.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
